b1c3172bbecc1a70adc3cd09fc53809fa84ff232
Mawethu Madanile <mamadjhb025@student.wethinkcode.co.za>
2026-09-16 14:26:54 +0200

    added alerts files

---
+++ alert-level-service/src/main/java/co/wethinkcode/healthsafe/AlertLevel.java
@@ -0,0 +1,12 @@
+package co.wethinkcode.healthsafe;
+
+public class AlertLevel {
+    public int level;
+    
+    public AlertLevel(){
+    }
+    public AlertLevel(int level){
+            this.level = level;
+        }
+    
+}
--- alert-level-service/src/main/java/co/wethinkcode/healthsafe/AlertLevelServiceApp.java
+++ alert-level-service/src/main/java/co/wethinkcode/healthsafe/AlertLevelServiceApp.java
@@ -1,15 +1,31 @@
 package co.wethinkcode.healthsafe;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import io.javalin.Javalin;
+import io.javalin.http.BadRequestResponse;
 
 public class AlertLevelServiceApp {
 
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(7032);
 
+        AtomicInteger alertLevel = new AtomicInteger(0);
+
         app.get("/health", ctx -> ctx.result("OK"));
 
-        // TODO (Tracks the hospital Emergency Status (0-8, 8 = full Code Blue).)
-        // Add domain endpoints for alert-level-service here.
+        app.get("/alert-Level", ctx -> {
+            ctx.json(new AlertLevel(alertLevel.get()));
+        });
+
+        app.put("/alert-level", ctx -> {
+            AlertLevel body = ctx.bodyAsClass(AlertLevel.class);
+            if (body.level < 0 || body.level > 8) {
+                throw new BadRequestResponse("level must be between ) and 8");
+            }
+            alertLevel.set(body.level);
+            ctx.json(new AlertLevel(alertLevel.get()));
+        });
+
     }
 }
